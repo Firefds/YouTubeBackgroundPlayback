02196cfe44d9c30be9e6840bc75900e79e4bc107
Shauli Bracha <shauli.bracha@hotmail.com>
2019-03-31 10:47:33 +0300

    Update

--- java/com/pyler/youtubebackgroundplayback/YouTubeBackgroundPlayback.java
+++ java/com/pyler/youtubebackgroundplayback/YouTubeBackgroundPlayback.java
@@ -44,7 +44,7 @@ public class YouTubeBackgroundPlayback implements IXposedHookLoadPackage {
 
 	private static final String APP_PACKAGE = "com.google.android.youtube";
 
-	private static final String HOOKS_DOWNLOAD_URL = "https://raw.githubusercontent.com/pylerSM/YouTubeBackgroundPlayback/master/assets/hooks-3.json";
+	private static final String HOOKS_DOWNLOAD_URL = "https://raw.githubusercontent.com/firefds/YouTubeBackgroundPlayback/master/assets/hooks-3.json";
 
 	private static final ScheduledExecutorService WORKER = Executors.newSingleThreadScheduledExecutor();
 
